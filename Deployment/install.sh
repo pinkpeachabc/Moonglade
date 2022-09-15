807e516d99db6ca469fcd30a269e9e0c4e3a2260
Mason Lee <44872954+pinkpeachabc@users.noreply.github.com>
2022-09-15 23:55:53 +0800

    Update install.sh
    fix

--- Deployment/install.sh
+++ Deployment/install.sh
@@ -7,7 +7,6 @@ install_Moonglade()
     apt-get update --allow-releaseinfo-change
     apt upgrade -y
     server="$1"
-    db_type="$2"
     
     aiur console/success "Checking..."
     # Valid domain is required
@@ -16,13 +15,6 @@ install_Moonglade()
         echo "IP is correct."
     else
         echo "$server is not your current machine IP!"
-        return 9
-    fi
-
-    # Valid database type mssql(default) and mysql 8.0
-    if [ "$db_type" != "mssql" ] && [ "$db_type" != "mysql" ];
-    then 
-        echo "$db_type is not supported database in this script! try mssql or mysql."
         return 9
     fi
 
@@ -41,12 +33,7 @@ install_Moonglade()
     aiur install/jq
     aiur install/dotnet
     aiur install/caddy
-    if [ "$db_type" == "mssql" ];
-    then
-        aiur install/sql_server $dbPassword
-    else
-        apt install mysql-server -y
-    fi
+    aiur install/sql_server $dbPassword
     #aiur install/node
 
     aiur console/success "Cloning..."
@@ -66,17 +53,8 @@ install_Moonglade()
 
     # Configure appsettings.json
     aiur console/success 'Configuring...'
+    connectionString="Server=tcp:127.0.0.1,1433;Database=Moonglade;uid=sa;Password=$dbPassword;MultipleActiveResultSets=True;"
 
-    # Configure different database type
-    if [ "$db_type" == "mssql" ];
-    then    
-        connectionString="Server=tcp:127.0.0.1,1433;Database=Moonglade;uid=sa;Password=$dbPassword;MultipleActiveResultSets=True;"
-        db_name="SqlServer"
-    else    
-        connectionString="Server=localhost;Port=3306;Database=Moonglade;uid=root;Password=$dbPassword;"
-        db_name="MySql"
-    fi
-    aiur text/edit_json "ConnectionStrings.DatabaseType" $db_name $moonglade_path/appsettings.Production.json
     aiur text/edit_json "ConnectionStrings.MoongladeDatabase" "$connectionString" $moonglade_path/appsettings.Production.json
     aiur text/edit_json "ImageStorage.FileSystemPath" '\/root\/Storage' $moonglade_path/appsettings.Production.json
     aiur text/edit_json "ImageStorage.FileSystemSettings.Path" '\/root\/Storage' $moonglade_path/appsettings.Production.json
@@ -84,15 +62,8 @@ install_Moonglade()
     #npm install web-push -g
 
     # Create database.
-    if [ "$db_type" == "mssql" ];
-    then
-        aiur console/success 'Seeding...'
-        aiur mssql/create_db "Moonglade" $dbPassword
-    else
-        # Initiate mysql root password and create database Moonglade
-        mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password by '$dbPassword'"
-        mysql -uroot -e "create database Moonglade" -p"$dbPassword"
-    fi
+    aiur console/success 'Seeding...'
+    aiur mssql/create_db "Moonglade" $dbPassword
 
     # Register Moonglade service
     aiur console/success "Registering..."
@@ -113,24 +84,14 @@ install_Moonglade()
 
     aiur console/success 'Finlization...'
     # Finish the installation
-    if [ "$db_type" == "mssql" ];
-    then
-        db_port="1433"   
-        db_user="sa"
-        db_data_dir="/var/opt/mssql/"
-    else   
-        db_port="3306"
-        db_user="root"
-        db_data_dir="/var/lib/mysql/"
-    fi
     echo "Successfully installed Moonglade as a service in your machine! Please open https://$server to try it now!"
     echo "Default management user name is "admin" and default password is "admin123". Please open https://$server/admin to try it now!"
-    echo "Successfully installed $db_type as a service in your machine! The port is not opened so you can't connect!"
+    echo "Successfully installed mssql as a service in your machine! The port is not opened so you can't connect!"
     echo "Successfully installed caddy as a service in your machine!"
     sleep 1
-    echo "You can open your database to public via: sudo ufw allow $db_port/tcp"
-    echo "You can access your database via: $server:$db_port with username: $db_user and password: $dbPassword"
-    echo "Your database data file is located at: $db_data_dir. Please back up them regularly."
+    echo "You can open your database to public via: sudo ufw allow 1433/tcp"
+    echo "You can access your database via: $server:1433 with username: sa and password: $dbPassword"
+    echo "Your database data file is located at: /var/opt/mssql/. Please back up them regularly."
     echo "Your web data file is located at: $moonglade_path"
     echo "Your web server config file is located at: /etc/caddy/Caddyfile"
     echo "Strongly maintain your own configuration at $moonglade_path/appsettings.Production.json"
